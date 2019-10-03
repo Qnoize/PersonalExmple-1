4709d13d066b825d3011b051461f6ca3e953c3ce
alekseydanilov <qnoize@gmail.com>
2019-10-04 00:12:54 +0300

    Initial commit

---
+++ src/main/java/servlet/DeleteUserServlet.java
@@ -0,0 +1,22 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+//@WebServlet ("/delete")
+public class DeleteUserServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.write("DeleteUserServlet!");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
